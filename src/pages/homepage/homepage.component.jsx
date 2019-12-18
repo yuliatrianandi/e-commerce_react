5412bc5f12d30763eaac8f6809cf6bdbbf8d596e
yuliatrianandi <yuliatrianandi@gmail.com>
2019-12-18 22:35:39 +0700

    change scss to jsx using styled component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,12 +2,12 @@ import  React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import './homepage.styles.scss';
+import { HomePageContainer } from './homepage.styles';
 
 const HomePage = ({ history }) => (
-    <div className='homepage'>
+    <HomePageContainer>
         <Directory history={ history } />
-    </div>
+    </HomePageContainer>
 )
 
 export default HomePage;
