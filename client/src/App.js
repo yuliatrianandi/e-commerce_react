cf8e171578a0b5c77a1563d9309bbf18214a44ac
yulia trianandi <yuliatrianandi@gmail.com>
2019-12-23 12:57:46 +0700

    implement lazy load and suspense

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import Header from './components/header/header.component';
+import Spinner from './components/spinner/spinner.component';
 
 import { GlobalStyle } from './global.styles';
 
@@ -26,7 +27,7 @@ const App = ({checkUserSession, currentUser}) => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Suspense fallback={<div>...Loading</div>}>
+        <Suspense fallback={<Spinner />}>
           <Route exact path='/' component={HomePage} />        
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
